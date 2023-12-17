9a8b26a7c94eb5f230efa53db8319ef5f3fde78a
TWTCT80 <115588691+TWTCT80@users.noreply.github.com>
2023-12-17 23:28:13 +0100

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.social-icons' , {delay:600, origin:'left'});
+sr.reveal('.home-img' , {delay:600, origin:'right'});
