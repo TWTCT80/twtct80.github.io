92350d50de1be5fb0553f5a1dfcd3c9e28109b65
TWTCT80 <115588691+TWTCT80@users.noreply.github.com>
2023-12-18 19:21:09 +0100

    new

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ const sr = ScrollReveal ({
     reset: true
 });
 
-sr.reveal('.social-icons' , {delay:600, origin:'left'});
-sr.reveal('.home-img' , {delay:600, origin:'right'});
+sr.reveal('.social-icons' , {delay:200, origin:'left'});
+sr.reveal('.home-img' , {delay:200, origin:'right'});
+sr.reveal('.aboutImg' , {delay:200, origin:'left'});
+sr.reveal('#aboutMe' , {delay:200, origin:'right'});
