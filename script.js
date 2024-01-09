927a80a933ec9e08ea2df3a542a852f201781c09
TWTCT80 <115588691+TWTCT80@users.noreply.github.com>
2024-01-09 15:49:16 +0100

    new update

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ sr.reveal('.social-icons' , {delay:200, origin:'left'});
 sr.reveal('.home-img' , {delay:200, origin:'right'});
 sr.reveal('.aboutImg' , {delay:200, origin:'left'});
 sr.reveal('#aboutMe' , {delay:200, origin:'right'});
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id + ' ]').classList.add('active');
+            })
+        }
+    })
+}
